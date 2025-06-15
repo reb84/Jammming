934cfc97b016e49c636635bc00c9fa7c0d2ebeb2
Reb84 <reb.upton84@gmail.com>
2025-06-15 17:02:13 +0100

    Create MockSpotifyData.js

---
+++ src/utils/MockSpotifyData.js
@@ -0,0 +1,90 @@
+// MockSpotifyData.js
+
+const mockSpotifyTracks = [
+  {
+    id: "1",
+    name: "Blinding Lights",
+    album: {
+      name: "After Hours",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b2736b1c6f945d28b1e3f3e47f67",
+        },
+      ],
+    },
+    artists: [{ name: "The Weeknd" }],
+    duration_ms: 200040,
+  },
+  {
+    id: "2",
+    name: "Levitating",
+    album: {
+      name: "Future Nostalgia",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b2730dfba0c6b0c749e3b898df75",
+        },
+      ],
+    },
+    artists: [{ name: "Dua Lipa" }],
+    duration_ms: 203064,
+  },
+  {
+    id: "3",
+    name: "Watermelon Sugar",
+    album: {
+      name: "Fine Line",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b273a3c3e13d1ea227fef32f202d",
+        },
+      ],
+    },
+    artists: [{ name: "Harry Styles" }],
+    duration_ms: 174000,
+  },
+  {
+    id: "4",
+    name: "As It Was",
+    album: {
+      name: "Harry’s House",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b2739f94f1e3cbb7ebd3722b2431",
+        },
+      ],
+    },
+    artists: [{ name: "Harry Styles" }],
+    duration_ms: 167000,
+  },
+  {
+    id: "5",
+    name: "Stay",
+    album: {
+      name: "Stay",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b2733f8d1a9e31c2be3e39cf2b46",
+        },
+      ],
+    },
+    artists: [{ name: "The Kid LAROI" }, { name: "Justin Bieber" }],
+    duration_ms: 141000,
+  },
+  {
+    id: "6",
+    name: "Save Your Tears",
+    album: {
+      name: "After Hours",
+      images: [
+        {
+          url: "https://i.scdn.co/image/ab67616d0000b2736b1c6f945d28b1e3f3e47f67",
+        },
+      ],
+    },
+    artists: [{ name: "The Weeknd" }],
+    duration_ms: 215000,
+  },
+];
+
+export default mockSpotifyTracks;
